6c6d9ec48752c47fd1d32fde8f3596c2577b3f06
Bijal Thanawala <bijalthanawala@users.noreply.github.com>
2022-09-01 12:20:30 +1000

    rudimentary CSV file reader. Important: ignores unicode characters

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
+from dataclasses import dataclass
 import sys
+import csv
 import argparse
 import logging
 import pprint
 from typing import List
 
-DEFAULT_CSV_FILENAME = "test.csv"
+DEFAULT_CSV_FILENAME = "data_10.csv"
 ALGORITHMS_LIST = {
     1: "Algorithm x",
     2: "Algorithm y",
     3: "Algorithm z",
 }
+
+# todo: Move this class to a different file later
+@dataclass
+class Result:
+    isSuccess: bool
+    info: str
+
+
+# todo: Start this method here initially and move this to a different file/module later
+def read_csv_file(csv_filename: str) -> Result:
+    try:
+        with open(
+            csv_filename, newline="", encoding="ascii", errors="ignore"
+        ) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                print(row)
+    except Exception as ex:
+        return Result(False, str(ex))
+
+        print()
+    return Result(True, "")
 
 
 def parse_arguments(unparsed_args: List[str]) -> argparse.Namespace:
@@ -47,9 +71,13 @@ def setup_logger(parsed_args: argparse.Namespace):
 def main(sys_argv: List[str]) -> bool:
     parsed_args = parse_arguments(sys_argv[1:])
     setup_logger(parsed_args)
-    pprint.pprint(parsed_args)
+    result = read_csv_file(parsed_args.file)
 
-    return True
+    if not result.isSuccess:
+        print(f"Error encountered: {result.info}")
+        print("Please fix the above error and rerun")
+
+    return result.isSuccess
 
 
 if __name__ == "__main__":
