588e4ee7a2f5fb8a0c6bc0219be8e25286ad7ab4
Bijal Thanawala <bijalthanawala@users.noreply.github.com>
2022-09-01 10:10:23 +1000

    Improved indentation/formatting and some minor changes

--- main.py
+++ main.py
@@ -4,39 +4,53 @@ import logging
 import pprint
 from typing import List
 
-DEFAULT_CSV_FILENAME="test.csv"
+DEFAULT_CSV_FILENAME = "test.csv"
 ALGORITHMS_LIST = {
-        1: "Algorithm x",
-        2: "Algorithm y",
-        3: "Algorithm z",
-        }
+    1: "Algorithm x",
+    2: "Algorithm y",
+    3: "Algorithm z",
+}
 
-def setup_argument_parser() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Trading Algorithm", formatter_class=argparse.RawTextHelpFormatter)
+
+def setup_argument_parser(unparsed_args: List[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Trading Algorithm", formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument(
+        "--file",
+        "-f",
+        default=DEFAULT_CSV_FILENAME,
+        help=f"Name of the input CSV file (Default={DEFAULT_CSV_FILENAME})",
+    )
+    parser.add_argument(
+        "--algorithm",
+        "-a",
+        type=int,
+        default=1,
+        choices=ALGORITHMS_LIST.keys(),
+        help=f"Specify one of the algorithm numbers (Default=1)\n{pprint.pformat(ALGORITHMS_LIST)}",
+    )
     parser.add_argument("--verbose", "-v", action="count", default=0)
-    parser.add_argument("--file", "-f", default=DEFAULT_CSV_FILENAME, help=f"Name of the input CSV file (Default={DEFAULT_CSV_FILENAME})")
-    parser.add_argument("--algorithm", "-a", type=int, default=1, choices= ALGORITHMS_LIST.keys(), help=f"Specify one of the algorithm numbers (Default=1)\n{pprint.pformat(ALGORITHMS_LIST)}")
-    args = parser.parse_args(sys.argv[1:])
-    return args
+    parsed_args = parser.parse_args(unparsed_args)
+    return parsed_args
 
-def setup_logger(args: argparse.Namespace):
+
+def setup_logger(parsed_args: argparse.Namespace):
     level_to_set = logging.WARNING
-    if args.verbose >= 2:
+    if parsed_args.verbose >= 2:
         level_to_set = logging.DEBUG
-    elif args.verbose == 1:
+    elif parsed_args.verbose == 1:
         level_to_set = logging.INFO
     logging.basicConfig(format="%(message)s", level=level_to_set)
 
-def usage(args) -> None:
-    print(f"Usage: {args[0]} <name of csv file>")
 
-def main(args: List[str]) -> bool:
-    args = setup_argument_parser()
-    setup_logger(args)
-    pprint.pprint(args)
+def main(sys_argv: List[str]) -> bool:
+    parsed_args = setup_argument_parser(sys_argv[1:])
+    setup_logger(parsed_args)
+    pprint.pprint(parsed_args)
 
     return True
 
 
 if __name__ == "__main__":
-    print(main(sys.argv))
+    main(sys.argv)
